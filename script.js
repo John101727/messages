35fec214bed882917eb677f361343f50f4ae3937
John101727 <johndavidremolleno017@gmail.com>
2025-02-14 02:50:55 +0800

    second commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('showPaperBtn').addEventListener('click', function() {
+    // Toggle the "show" class on the paper element to trigger the sliding effect
+    const paper = document.getElementById('paperContainer');
+    paper.classList.toggle('show');
+});
+
